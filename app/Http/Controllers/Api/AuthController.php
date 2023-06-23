025cb1cd5660a60208deb63d13147bfd6bf1301f
Themainbased <ardikece17@gmail.com>
2023-06-23 21:00:11 +0700

    Tahap 5 : Login Api Config

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
 
@@ -30,6 +31,28 @@ class AuthController extends Controller
             'token' => $token
         ], 200);
     }
+
+    public function login(Request $request) {
+        $request->validate([
+            'email'=>'required',
+            'password'=> ['required'],
+        ]);
+            if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+                $user = User::where('email', $request->email)->first();
+                $token = $user->createToken('token-name', ['server:update'])->plainTextToken;
+                return response()->json([
+                   'message' => 'Login Complete, Dont forgot your account',
+                   'data' => $user,
+                   'token' => $token
+                ], 401);
+            }else{
+                return response()->json([
+                    'message' => 'Email And Password Wrong, Check Again',
+                ], 401);
+            }
+            
+    }
+
     public function logout() {
         auth()->user()->tokens()->delete();
 
@@ -37,4 +60,6 @@ class AuthController extends Controller
             'message' => 'Logout Complete',
         ], 200);
     }
+
+
 }
